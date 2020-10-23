0b1795520459e1301d07d3ae543f2027c34db8d1
Take327 <gatewayseed2@gmail.com>
2020-10-23 22:44:25 +0900

    コメント変更

--- functions/index.js
+++ functions/index.js
@@ -21,8 +21,8 @@ let responseObject = {
         package_type: null,             //荷物種類
         package_size: null,             //荷物サイズ
         scheduled_date: null,           //お届け予定日
-        specified_date: null,            //お届け指定日
-        specified_time: null,            //お届け指定時間
+        specified_date: null,           //お届け指定日
+        specified_time: null,           //お届け指定時間
         tracking_info: {}               //追跡情報
     }
 };
@@ -45,10 +45,15 @@ async function getTrackingInfoJapanPost(requestNo) {
 
 };
 
+/**
+ * domデータを成形しオブジェクトに格納する。
+ * @param {string} jpTrackingInfo 
+ * @param {string} deliveryCarrierCode 
+ */
+
 function createTrakingInfo(jpTrackingInfo, deliveryCarrierCode) {
     if (deliveryCarrierCode == 'JP') {
         //JP追跡情報成形
-        const obj = {}
         for (let i = 0, row = 1; jpTrackingInfo.length > i; i += 6, row++) {
             responseObject.data.tracking_info[`row${row}`] = {
                 data: jpTrackingInfo[i],
@@ -60,6 +65,10 @@ function createTrakingInfo(jpTrackingInfo, deliveryCarrierCode) {
             };
         }
         return responseObject
+    } else if (deliveryCarrierCode == 'Yamato') {
+        //Yamato追跡情報成形
+    } else {
+        //Sagawa追跡情報成形
     }
 }
 
@@ -131,6 +140,16 @@ function trackingNumberMatchCheck(trackingNumber) {
     }
 }
 
+/**
+ * リクエストが成功した際のオブジェクト更新
+ * @returns void
+ * code:200
+ */
+function setSuccessMeta(){
+    responseObject.meta.code = 4001;
+    responseObject.meta.type = 'Success';
+    responseObject.meta.message = 'The request was successful.';
+}
 
 /**
  * delivery_carrier_codeが設定されていない場合のオブジェクト更新
